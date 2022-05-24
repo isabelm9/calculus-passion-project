4895096da9e9f9c7e080453b90b2be33b098ec86
isabelm9 <isabel.maida@gmail.com>
2022-05-24 10:08:34 -0400

    init

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/0')
+@app.route('/news')
 def news():
     return render_template("news.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/games')
+def games():
+    return render_template("games.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("getinvolved.html")
